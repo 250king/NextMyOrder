e01d55f920e115f38dba7963ee3db71601b6baa6
250king <2789260300@qq.com>
2025-09-21 04:49:02 +0800

    formate

--- packages/util/src/data/string.ts
+++ packages/util/src/data/string.ts
@@ -3,23 +3,23 @@ export const cStd = (value: number) => {
         style: "currency",
         currency: "JPY",
     }).format(Number(value));
-}
+};
 
 export const mStd = (value: number | null) => {
     return new Intl.NumberFormat("zh-CN", {
         style: "unit",
         unit: "gram",
-        unitDisplay: "short"
-    }).format(value || 0)
-}
+        unitDisplay: "short",
+    }).format(value || 0);
+};
 
 export const rStd = (value: number) => {
     return new Intl.NumberFormat("zh-CN", {
         style: "percent",
         minimumFractionDigits: 2,
-    }).format(value)
-}
+    }).format(value);
+};
 
 export const jStd = (value: string) => {
-    return value.replace(/[\u3000\u00A0]/g, ' ').trim()
-}
+    return value.replace(/[\u3000\u00A0]/g, ' ').trim();
+};
