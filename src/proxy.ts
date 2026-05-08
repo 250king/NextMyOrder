03f4ddf18a5e7ba3e11504db6dd0d7f4621e2c7e
250king <250king@gochiusa.cafe>
2026-05-08 16:03:11 +0800

    Fix cookie secure problem

--- src/proxy.ts
+++ src/proxy.ts
@@ -64,9 +64,9 @@ export const proxy = async (request: NextRequest) => {
         });
     }
     response.cookies.set({
-        name: env.SESSION_COOKIE,
+        name: env.SESSION_COOKIE_NAME,
         value: sid,
-        secure: env.NODE_ENV === "production",
+        secure: env.SESSION_COOKIE_SECURE ? env.SESSION_COOKIE_SECURE === "true" : env.NODE_ENV === "production",
         maxAge: env.SESSION_TTL,
         httpOnly: true,
         sameSite: "lax",
